62c67e8a4bf99dc4c3bb8e9b3f2f1078170f427c
DominickPortella <157386679+DominickPortella@users.noreply.github.com>
2024-06-13 09:53:55 -0500

    new

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-import { config as dotenv } from "dotenv";
-dotenv();
+import dotenv from "dotenv";
+dotenv.config();
 
 export const config = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: 3306
+  port: process.env.DB_PORT || 3306,
 };
